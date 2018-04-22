2d0e26a546ba16c9a676846e10406001577aa769
Joshua Ashton <joshua@nonagon.games>
2018-04-22 20:39:47 +0100

    Merge upstream dx11 changes to dx10

--- src/d3d10_1/d3d10_1_device.cpp
+++ src/d3d10_1/d3d10_1_device.cpp
@@ -412,7 +412,7 @@ namespace dxvk {
           break;
           
         case D3D10_SRV_DIMENSION_TEXTURE2DMS:
-          viewInfo.type      = VK_IMAGE_VIEW_TYPE_2D;
+          viewInfo.type      = VK_IMAGE_VIEW_TYPE_2D_ARRAY;
           viewInfo.minLevel  = 0;
           viewInfo.numLevels = 1;
           viewInfo.minLayer  = 0;
