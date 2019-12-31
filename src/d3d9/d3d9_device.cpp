3550082af45c6730ce665de848a46e36b843f5da
Joshua Ashton <joshua@froggi.es>
2019-12-31 21:51:12 +0000

    [meme] PS1-style graphics
    No perspective messing though as that completely breaks some titles

--- src/d3d9/d3d9_device.cpp
+++ src/d3d9/d3d9_device.cpp
@@ -5178,15 +5178,14 @@ namespace dxvk {
       colorInfo.addressModeW   = DecodeAddressMode(cKey.AddressW);
       colorInfo.compareToDepth = VK_FALSE;
       colorInfo.compareOp      = VK_COMPARE_OP_NEVER;
-      colorInfo.magFilter      = DecodeFilter(cKey.MagFilter);
-      colorInfo.minFilter      = DecodeFilter(cKey.MinFilter);
+      colorInfo.magFilter      = VK_FILTER_NEAREST;
+      colorInfo.minFilter      = VK_FILTER_NEAREST;
       colorInfo.mipmapMode     = mipFilter.MipFilter;
-      colorInfo.maxAnisotropy  = float(cKey.MaxAnisotropy);
-      colorInfo.useAnisotropy  = IsAnisotropic(cKey.MinFilter)
-                              || IsAnisotropic(cKey.MagFilter);
+      colorInfo.maxAnisotropy  = 1.0f;
+      colorInfo.useAnisotropy  = false;
       colorInfo.mipmapLodBias  = cKey.MipmapLodBias;
-      colorInfo.mipmapLodMin   = mipFilter.MipsEnabled ? float(cKey.MaxMipLevel) : 0;
-      colorInfo.mipmapLodMax   = mipFilter.MipsEnabled ? FLT_MAX                 : 0;
+      colorInfo.mipmapLodMin   = 4;
+      colorInfo.mipmapLodMax   = std::max(colorInfo.mipmapLodMin, FLT_MAX);
       colorInfo.usePixelCoord  = VK_FALSE;
       for (uint32_t i = 0; i < 4; i++)
         colorInfo.borderColor.float32[i] = cKey.BorderColor[i];
